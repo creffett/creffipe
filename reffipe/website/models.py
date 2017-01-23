9f2ff7978b1423a2e5704f121e1dc708b3447c51
Chris Reffett <cmreffett@gmail.com>
2017-01-22 21:10:03 -0500

    Validation fixes

--- reffipe/website/models.py
+++ reffipe/website/models.py
@@ -9,18 +9,22 @@ MEASUREMENT = (
     ('item', 'item'),
 )
 
+
 class Step(models.Model):
     """A step in a recipe"""
     order = models.IntegerField()
     directions = models.TextField()
     recipe = models.ForeignKey('Recipe')
 
+
 class Tag(models.Model):
     """A tag to identify a recipe"""
     name = models.CharField(max_length=50)
     recipes = models.ManyToManyField('Recipe')
+
     def __str__(self):
         return self.name
+
 
 class RecipeIngredient(models.Model):
     ingredient = models.ForeignKey('Ingredient')
@@ -30,12 +34,16 @@ class RecipeIngredient(models.Model):
     description = models.CharField(max_length=100)
     notes = models.TextField()
 
+
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
+
     def __str__(self):
         return self.name
 
+
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+
     def __str__(self):
         return self.name
--- reffipe/website/views.py
+++ reffipe/website/views.py
@@ -5,6 +5,7 @@ from django.template import loader
 from .models import Recipe
 # Create your views here.
 
+
 def index(request):
     recipe_list = Recipe.objects.order_by('name')
     template = loader.get_template('reffipe/index.html')
@@ -12,6 +13,7 @@ def index(request):
         'recipe_list': recipe_list,
     }
     return HttpResponse(template.render(context, request))
+
 
 def recipe(request, recipe_id):
     return HttpResponse("You're looking at recipe %s." % recipe_id)
